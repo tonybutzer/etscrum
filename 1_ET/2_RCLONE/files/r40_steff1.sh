d3fa6d0b382215bbac27cdb9bf8ec6a154230ed3
tonybutzer <tonybutzer@gmail.com>
2021-04-15 16:49:37 +0000

    automatic git update from Makefile

--- 1_ET/2_RCLONE/files/r40_steff1.sh
+++ 1_ET/2_RCLONE/files/r40_steff1.sh
@@ -6,4 +6,4 @@ ssh-add -k ~/.ssh/id_rsa
 
 rclone listremotes
 
-rclone copy -P et-data:dev-et-data/out/CONUS/Run03_30_2021/run_r40tile6_1999_2019_r40.0_tile6_chip36.0N-104.2E/2001/ denali:/caldera/projects/usgs/water/impd/butzer/run_r40tile6_1999_2019_r40.0_tile6_chip36.0N-104.2E/2001/ &
+rclone copy -P et-data:dev-et-data/out/CONUS/Run03_30_2021/run_r40tile6_1999_2019_r40.0_tile6_chip36.0N-104.2E/2007/ denali:/caldera/projects/usgs/water/impd/butzer/run_r40tile6_1999_2019_r40.0_tile6_chip36.0N-104.2E/2007/ &
